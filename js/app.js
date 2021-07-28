2f83cb3a477dca37b9e096fbe11720c77e3df031
alekseitb <aleksei_tb@mail.ru>
2021-07-28 12:02:30 +0300

    main

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
 console.log("Start");
+
 const button = document.getElementById("btn");
+const color = document.querySelector(".color")
+
 console.log(button);
+
+//  типы цветов hexadecimal colors
+const colors = ["#FF5733", "#F9FF33", "#3371FF", "red", "green", "rgb(85,255,51)"]
+
 
 button.addEventListener("click", () => {
 	console.log("click");
+	console.log(getRandomNumber());
+
+	let hexColor = colors[getRandomNumber()];
+	document.body.style.backgroundColor = hexColor;
+	color.textContent = hexColor;
 })
 
+function getRandomNumber() {
+	return Math.floor(Math.random() * colors.length);
+}
